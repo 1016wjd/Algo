5df9df583e783e158793bf2d33bb69498a19df94
1016wjd <1016wjd@naver.com>
2024-01-16 23:51:10 +0900

    0116_python_2

---
+++ programmers/n번째원소부터/sol.py
@@ -0,0 +1,4 @@
+num_list = [2, 1, 6]
+n = 3
+
+print(num_list[n-1:])
---
+++ programmers/rny_string/sol.py
@@ -0,0 +1,8 @@
+rny_string = "masterpiece"
+
+def solution(rny_string):
+    answer = rny_string.replace("m","rn")
+    return answer
+
+
+print(solution(rny_string))
